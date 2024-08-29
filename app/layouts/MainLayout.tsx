b7ea5720c6dae3852e5965c98f9b825afcf19594
Mohammad Ali Alaawar <m.alialaawar@gmail.com>
2024-08-29 13:21:36 +0300

    feat: 🎸 created mainLayout

---
+++ app/layouts/MainLayout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '@/app/components/Navbar';
+import Footer from '@/app/components/footer';
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
