0ba74dda16420197362a135c25464c02705774b5
Mohammad Ali Alaawar <m.alialaawar@gmail.com>
2024-08-29 13:23:06 +0300

    fix: 🐛 used the mainLayout component

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from './layouts/MainLayout';
+import HeroSection from './components/HeroSection';
+
+const HomePage: React.FC = () => {
+  return (
+    <MainLayout>
+      <HeroSection />
+    </MainLayout>
+  );
+};
+
+export default HomePage;
