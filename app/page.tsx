0ec4267ab1c7474f21124ff537d13b1c5e84f3d8
5-07 <muzainamunirsiddiqui@gmail.com>
2024-09-04 09:59:34 +0400

    added preview on hero

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import { Hero } from "@/components/ui/Hero";
 import { Features } from "@/components/ui/Features";
 import { FAQs } from "@/components/ui/FAQs";
 import { CallToAction } from "@/components/ui/CallToAction";
-import Footer from "./components/Footer";
+import { Footer } from "@/components/ui/Footer";
+import { DashPreview } from '@/components/ui/Dashpreview';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
     <Header/>
     <Hero />
     <Features />
+    <DashPreview />
     <FAQs />
     <CallToAction />
     <Footer />
