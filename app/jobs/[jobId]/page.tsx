76f1a2003c66f77fcdadd9f41d5f708eecdd0dbe
5-07 <muzainamunirsiddiqui@gmail.com>
2024-09-09 19:47:27 +0400

    Made changes to the UI

--- app/jobs/[jobId]/page.tsx
+++ app/jobs/[jobId]/page.tsx
@@ -8,8 +8,9 @@ import { doc, getDoc, onSnapshot, updateDoc, writeBatch } from 'firebase/firesto
 import { Loader } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
-
-
+import Image from "next/image";
+import Logo from "@/assets/images/traj_logo_blackR-removebg-preview.png"; 
+import { Footer } from "@/components/ui/Footer";
 
 
 const JobPage = ({params} : {params: {jobId: string}}) => {
@@ -21,22 +22,19 @@ const JobPage = ({params} : {params: {jobId: string}}) => {
   const [jobData, setJobData] = useState<JobData | null>(null);
   const jobId = params.jobId;
   const [skillAssessments, setSkillAssessments] = useState<SkillAssessments | null>(null);
-
-
   const router = useRouter();
 
   useEffect(() => {
     setTargetJob(localStorage.getItem('targetJob'));
 
     const targetJobMatchesString = localStorage.getItem('targetJobMatches');
-
     const targetJobMatches: MatchData[] = targetJobMatchesString ? JSON.parse(targetJobMatchesString) : [];
     setTargetJobMatches(targetJobMatches);
 
     const topMatchesString = localStorage.getItem('topMatches');
     const topMatches: MatchData[] = topMatchesString ? JSON.parse(topMatchesString) : [];
     setTopMatches(topMatches);
-    // check if job is in localstorage
+
     let allMatches = [];
     allMatches.push(...(targetJobMatches ?? []));
     allMatches.push(...(topMatches ?? []));
@@ -99,14 +97,10 @@ const JobPage = ({params} : {params: {jobId: string}}) => {
     }
   }, [isLoaded, isSignedIn, user, router]);
 
-  // get job title required skills and experience
-  // get user skills assessments, default to 1 if not in firestore
   useEffect(() => {
     const getMissingSkillAssessments = async () => {
       if (isLoaded && isSignedIn && user && jobData) {
         try {
-          
-          // get all skills assessments from fire store
           const userDocRef = doc(db, 'users', user.id);
           const userDocSnap = await getDoc(userDocRef);
           if (userDocSnap.exists()) {
@@ -114,18 +108,10 @@ const JobPage = ({params} : {params: {jobId: string}}) => {
             if (!data.assessments) {
               data.assessments = {};
             }
-            console.log('getSkillAssessments skillAssessments: ', data.assessments);
-            // find skills in jobData.skills that are not in firestore
             const currentAssessments : SkillAssessments = data.assessments;
-            console.log('getSkillAssessments assessments: ', currentAssessments);
-            // console.log('getSkillAssessments assessments["AWS"]: ', currentAssessments["AWS"]);
             const missingSkills = jobData.skills.filter((skill) => !(skill in currentAssessments));
-            // setMissingSkillAssessments(missingSkills);
-            console.log('getSkillAssessments missingSkills: ', missingSkills);
             if (missingSkills && missingSkills.length > 0) {
-              // if missingSkills.length > 0, fetch userData from firestore
               const userData = data.userData;
-              // send userData, missingSkills to createSkillAssessment endpoint 
               const response = await axios.post(
                 `/api/createAssessments`,
                 {
@@ -134,11 +120,8 @@ const JobPage = ({params} : {params: {jobId: string}}) => {
                 }
               )
               const missingSkillAssessments = response.data.assessments;
-              console.log('getSkillAssessments missingSkillAssessments: ', response.data.assessments);
-              // store returned assessments in firestore
               const batch = writeBatch(db)
               for (const [skill, score] of missingSkillAssessments) {
-                console.log("skill score", skill, score);
                 const assessment : SkillAssessment = {
                   name: skill,
                   score,
@@ -148,8 +131,6 @@ const JobPage = ({params} : {params: {jobId: string}}) => {
                 currentAssessments[skill] = assessment;
               }
               await batch.commit();
-              // set skillAssessments
-              console.log('getSkillAssessments currentAssessments: ', currentAssessments);
             }
             setSkillAssessments(currentAssessments);
           }
@@ -157,13 +138,9 @@ const JobPage = ({params} : {params: {jobId: string}}) => {
         catch (error) {
           console.log('getSkillAssessments error: ', error);
         }
-
-
       }
     }
-    // if (isLoaded && isSignedIn && jobData && jobData.skills.length > 0) {
-      getMissingSkillAssessments();
-    // }
+    getMissingSkillAssessments();
   }, [isLoaded, isSignedIn, user, jobData]);
 
   const toggleSkillStatus = async (skill: string) => {
@@ -178,116 +155,93 @@ const JobPage = ({params} : {params: {jobId: string}}) => {
     }
   }
 
-
-
-
-
-
+  // If job data and skill assessments are not loaded, display loading
   if (jobData && jobData.skills.length > 0 && !skillAssessments) {
     return <div className='flex justify-center items-center h-screen'><Loader className="w-12 h-12 animate-spin" /></div>;
-
-
-
-
   }
-  
 
+  // Score Bar Calculation
+  const calculateOverallScore = () => {
+    if (!skillAssessments || !jobData) return 0;
+    const totalSkills = jobData.skills.length;
+    const completedSkills = jobData.skills.filter(skill => skillAssessments[skill]?.status === 'complete').length;
+    return Math.round((completedSkills / totalSkills) * 100);
+  };
 
   return (
-    // Display a specific job and its required skills and experience
-    // compare with user's skills and experience?
     <>
-    <Header />
-    <div className="flex flex-col items-center pt-20">
-      <h1 className="text-3xl font-bold mb-4">{jobData && jobData.title}</h1>
-    </div>
-    <div
-
-    >
-      {jobData && jobData.skills.length > 0 && (
-        <>
-        <div
-          className='max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8'
-        >
-          <div className="flex flex-col ">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <div
-              className="flex justify-between"
-            >
-              <h2 className="text-xl font-bold mb-2 pl-12">Skills</h2>
-              <h3 
-                className="text-xl font-bold mb-2"
-
-
-
-
-
-
-              >
-                Assessments
-              </h3>
-            </div>
-            <ul>
-              {skillAssessments && jobData && jobData.skills.map((skill, index) => (
-                <div
-                  key={skillAssessments[skill].name}
-                  className="flex justify-between items-center border border-white/15 group hover:bg-gradient-to-br from-slate-50 via-violet-200 to-slate-50 transition duration-300 ease-in-out p-4 rounded-lg"
-                >
-                  <div className="flex items-center space-x-4">
-                    <input
-                      type="checkbox"
-                      checked={skillAssessments[skill].score > 3 || skillAssessments[skill].status === 'complete'}
-                      onChange={() => toggleSkillStatus(skill)}
-                      className="h-6 w-6 text-green-500 focus:ring-green-500 border-gray-300 rounded"
-                    />
-                    <div>
-                      <h2 className="text-xl group-hover:text-black transition duration-300 ease-in-out">{skillAssessments[skill].name}</h2>
-                      <p className="text-gray-400 text-sm group-hover:text-black transition duration-300 ease-in-out">{skillAssessments[skill]['status']}</p>
-                    </div>
-                  </div>
-                  <span
-                    className={`${
-                      skillAssessments[skill].score > 3 || skillAssessments[skill].status === 'complete'
-                        ? "text-green-500/70"
-                        : "text-red-500/70"
-                    } font-bold`}
-                  >
-                    {skillAssessments[skill].score}
-                  </span>
-                </div>
-                // <li key={index} className="text-lg mb-2">{skill} : {skillAssessments && skillAssessments[skill]['score']}</li>
-              ))}
-            </ul>
+      <Header />
+      <header className="bg-gradient-to-br from-purple-200 to-purple-300 p-4 border-b border-black shadow-md">
+        <div className="container mx-auto flex items-center justify-between">
+          <Image src={Logo} alt="SaaS Logo" height={150} width={150} />
           </div>
-
-
+          </header>
+      <div className="flex flex-col items-center pt-20">
+        <h1 className="text-5xl font-bold bg-gradient-to-r from-purple-800 via-purple-400 to-purple-300 bg-clip-text text-transparent">
+          Welcome!
+        </h1>
+        <h1 className="text-xl font-bold mb-4 mt-4">{jobData && jobData.title}</h1>
+        {/* Centered Score Bar */}
+        <div className="w-full max-w-4xl px-4 sm:px-6 lg:px-8 mb-6">
+          <div className="bg-gray-300 h-4 rounded-lg">
+            <div
+              className="bg-purple-950 h-4 rounded-lg"
+              style={{ width: `${calculateOverallScore()}%` }}  // Dynamically set the width based on the overall score
+            ></div>
+          </div>
+          <p className="text-right mt-2">{calculateOverallScore()}% complete</p>
         </div>
-        </>
-      )}
-    </div>
+        {/* Job Title */}
+      </div>
+      <div>
+        {jobData && jobData.skills.length > 0 && (
+          <>
+            <div className='max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
+              <div className="flex flex-col ">
+                <div className="flex justify-between">
+                  <h2 className="text-xl font-bold mb-2 pl-6">Skills</h2>
+                  <h3 className="text-xl font-bold mb-2">Assessments</h3>
+                </div>
+                <ul>
+                  {skillAssessments && jobData && jobData.skills.map((skill) => (
+                    <div
+                      key={skillAssessments[skill].name}
+                      className="flex justify-between items-center border border-gray-300 group hover:bg-gradient-to-br from-slate-50 via-violet-200 to-slate-50 transition duration-300 ease-in-out p-4 rounded-lg mb-4"
+                    >
+                      <div className="flex items-center space-x-6">
+                        <input
+                          type="checkbox"
+                          checked={skillAssessments[skill].score > 3 || skillAssessments[skill].status === 'complete'}
+                          onChange={() => toggleSkillStatus(skill)}
+                          className="h-6 w-6 text-green-500 focus:ring-green-500 border-gray-300 rounded"
+                        />
+                        <div>
+                          <h2 className="text-xl group-hover:text-black transition duration-300 ease-in-out">{skillAssessments[skill].name}</h2>
+                          <p className="text-gray-400 text-sm group-hover:text-black transition duration-300 ease-in-out">{skillAssessments[skill]['status']}</p>
+                        </div>
+                      </div>
+                      <span
+                        className={`${
+                          skillAssessments[skill].score > 3 || skillAssessments[skill].status === 'complete'
+                            ? "text-green-500/70"
+                            : "text-red-500/70"
+                        } font-bold`}
+                      >
+                        {skillAssessments[skill].score}
+                      </span>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+      <Footer />
     </>
-  )
+  );
+  
+  
 }
 
 export default JobPage;
-
-
